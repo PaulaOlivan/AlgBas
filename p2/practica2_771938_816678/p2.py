59e9d91752a710e0d6445741fb26d6d004e77143
Paula Olivan <771938@unizar.es>
2023-04-20 18:14:15 +0200

    Actualización P2
    Se lee de pruebas.txt y se escribe en resultados.txt

--- p2/practica2_771938_816678/p2.py
+++ p2/practica2_771938_816678/p2.py
@@ -59,42 +59,57 @@ def bolasFB (i, nodo):
         camino.extend(bolasFB(i, nodo.right))
         return camino
 
-if len(sys.argv) != 4:
-    print("Invocar como: p2.py profundidad i_bola usar_fuerza_bruta")
+if len(sys.argv) != 3:
+    print("Invocar como: p2.py pruebas.txt resultados.txt")
     exit(1)
 
-profundidad = int(sys.argv[1])
-i_bola = int(sys.argv[2])
-usar_fuerza_bruta = bool(int(sys.argv[3]))
+# Leemos linea a linea el archivo de entrada que contiene la profundidad y las bolas a colocar
+with open('pruebas.txt', 'r') as file_in:
+    with open('resultados.txt', 'w') as file_out:
+        # Ignoramos las dos primeras lineas del archivo de entrada que forman la cabecera
+        lines = file_in.readlines()[2:]
+        # Ecribimos la linea de cabecera
+        file_out.write("PosicionBolasLanzadas\tFuerzaBruta\tDivideYVenceras\tGPU")
+
+        for line in file_in:
+            prof, bol, *resto = line.split() # separar la línea en palabras
+            profundidad = int(prof) # convertir el primer número en entero
+            bolas = int(bol) # convertir el segundo número en entero
 
 
-nBolas = (1 << profundidad-1) #El numero total de bolas es el numero de nodos en la capa de las hojas
+            nBolas = (1 << profundidad-1) # El numero total de bolas es el numero de nodos en la capa de las hojas
 
-init = time.time()
-camino_cpu = dondeEstaLaBolita(profundidad, i_bola)
-finAlgoritmo = time.time()
-
-print ("Tiempo de ejecucion del algoritmo en CPU:", (finAlgoritmo-init))
-
-
-if usar_fuerza_bruta:
-    init = time.time()
-    raiz = create_complete_tree(profundidad, 0, 1)
-    camino_fb = colocarBolas(nBolas, raiz, i_bola)
-    finFB = time.time()
-    print ("Tiempo de ejecucion del metodo de fuerza bruta:", (finFB-init))
-    
-    if camino_cpu == camino_fb:
-        print("Los caminos son iguales")
-    else:
-        print("Los caminos son diferentes")
-        print("Camino CPU:", camino_cpu)
-        print("Camino FB:", camino_fb)
+            # Ejecutamos el algoritmo de divide y venceras
+            init = time.time()
+            camino_cpu = dondeEstaLaBolita(profundidad, bolas)
+            finAlgoritmo = time.time()
+            
+            # Mostramos tiempo por pantalla
+            print ("Tiempo de ejecucion del algoritmo en CPU:", (finAlgoritmo-init))
 
 
-init = time.time()
-#camino = p2_gpu.donde_esta_la_bolita_gpu(profundidad, i_bola)
-finAlgoritmo = time.time()
+            # Ejecutamos el algoritmo de fuerza bruta
+            init = time.time()
+            raiz = create_complete_tree(profundidad, 0, 1)
+            camino_fb = colocarBolas(nBolas, raiz, bolas)
+            finFB = time.time()
+            print ("Tiempo de ejecucion del metodo de fuerza bruta:", (finFB-init))
+            
+            if camino_cpu == camino_fb:
+                print("Los caminos son iguales")
+            else:
+                print("Los caminos son diferentes")
+                print("Camino CPU:", camino_cpu)
+                print("Camino FB:", camino_fb)
+            
+            # Escribimos el resultado en el archivo de salida
+            file_out.write(str(bolas) + "\t" + str(finFB-init) + "\t" + str(finAlgoritmo-init) + "\n")
 
-print ("Tiempo de ejecucion del algoritmo en GPU:", (finAlgoritmo-init))
+
+            #init = time.time()
+            #camino = p2_gpu.donde_esta_la_bolita_gpu(profundidad, i_bola)
+            #finAlgoritmo = time.time()
+
+            #print ("Tiempo de ejecucion del algoritmo en GPU:", (finAlgoritmo-init))
+
 
