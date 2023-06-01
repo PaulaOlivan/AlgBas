2500fb38272b1310a769116a7fa14ad10897c84b
hsunekichi <hsunekichi@gmail.com>
2023-06-01 17:48:47 +0200

    .

--- p4/ejecutar.sh
+++ p4/ejecutar.sh
@@ -19,10 +19,19 @@ then
     echo "Lanzamos el programa para probar los datos de pruebas.txt"
     python3 transporte.py pruebas.txt
 
+    # Probamos el programa con los datos en prueba en PL
+    echo "Lanzamos el programa para probar los datos de pruebas.txt con PL"
+    python3 transporte_pl.py pruebas.txt
+
+    # Probamos el programa con los datos de prueba en profundidad 
+    #echo "Lanzamos el programa para probar los datos de pruebas.txt en profundidad"
+    #python3 transporte_profundidad.py pruebas.txt
+
+
     # Probamos el programa con los datos incorrectos para comprobar los casos 
     # de error
     #echo "Lanzamos el programa para probar las pruebas de error de errores.txt"
-    #python3 transporte.py pruebas.txt
+    #python3 transporte.py errores.txt
 
     # Probamos el programa con los datos de rendimeinto para comprobar la eficiencia 
     # de nuestros programas
@@ -30,9 +39,5 @@ then
     #python3 transporte.py rendimiento.txt
     #python3 transporte_pl.py rendimiento.txt
     #python3 transporte_profundidad.py rendimiento.txt
-
-    # Probamos el programa con los datos en prueba en PL
-    echo "Lanzamos el programa para probar los datos de pruebas.txt con PL"
-    python3 transporte_pl.py pruebas.txt
 
 fi
